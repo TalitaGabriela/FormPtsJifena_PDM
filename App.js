3f6040c833a90826e34539d47eaf74d61af25bcb
TalitaGabriela <talitagabriela1216@gmail.com>
2024-04-10 15:44:55 -0400

    Commit
    Dando função a aplicação

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView, Pressable, TouchableOpacity} from "react-native";
 
 // HEADER
@@ -14,8 +15,51 @@ function Header() {
 
 // Início a aplicação:
 export default function App() {
-  return (
-   
+  // Definindo estados para os diferentes valores no aplicativo
+  const [esporte, setEsporte] = useState('');
+  const [time1, setTime1] = useState('');
+  const [time2, setTime2] = useState('');
+  const [jogadores, setJogadores] = useState([]);
+  const [selecao, setSelecao] = useState(null);
+  const [nome, setNome] = useState('');
+  const [camiseta, setCamiseta] = useState('');
+  const [placarT1, setPlacarT1] = useState(0);
+  const [placarT2, setPlacarT2] = useState(0);
+
+  // Função para adicionar um jogador
+  const addJogador = (nome, camiseta) => {
+    // Verifica se o time foi selecionado e se o nome ou número do jogador estão vazios
+    if (selecao === null || nome.trim() === '' || camiseta.trim() === '') {
+      return;
+    }
+    // Cria um novo jogador com os dados fornecidos
+    const novoJogador = { nome, camiseta, pontos: 0 };
+    // Atualiza o estado dos jogadores adicionando o novo jogador
+    setJogadores([...jogadores, novoJogador]);
+    setNome('');
+    setCamiseta('');
+  };
+
+   // Função para incrementar os pontos de um jogador
+   const addPontos = (index) => {
+    // Copia a lista de jogadores para atualização
+    const attJogadores = [...jogadores];
+    // Acessa o jogador atual
+    const nome = attJogadores[index];
+    // Incrementa os pontos do jogador atual
+    nome.pontos++;
+    // Atualiza o placar do time correspondente
+    if (selecao === 1) {
+      setPlacarT1(placarT1 + 1);
+    } else if (selecao === 2) {
+      setPlacarT2(placarT2 + 1);
+    }
+    // Atualiza o estado dos jogadores
+    setJogadores(attJogadores);
+  };
+
+
+  return ( 
         <ScrollView>
           <View>
 
@@ -26,7 +70,7 @@ export default function App() {
 
             {/* Input 1 - DIGITE O ESPORTE */}
             <View style={{ paddingHorizontal: 25, paddingVertical: 10 }}>
-              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }}
+              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }} value={esporte}
                 placeholder="Digite o Esporte"/>
             </View>
 
@@ -38,7 +82,7 @@ export default function App() {
 
             {/* Input 2 - Time 1 */}
             <View style={{ paddingHorizontal: 25 }}>
-              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }}
+              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }} value={time1}
                 placeholder="(Teste)"/>
             </View>
 
@@ -49,23 +93,23 @@ export default function App() {
 
             {/* Input 3 - Time2 */}
             <View style={{ paddingHorizontal: 25 }}>
-              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }}
+              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }} value={time2}
                 placeholder="(Teste)"
               />
             </View>
 
             {/* Placar */}
             <Text style={{ textAlign: "center", paddingTop: 15, fontSize: 22, fontWeight: "bold",}}>
-              Placar-(teste)
+              Placar-{esporte}
             </Text>
 
             <View style={{ flex: 1, justifyContent: "space-between", flexDirection: "row",}}>
-              <Text style={{ paddingHorizontal: 25, fontSize: 20 }}>
-                (Teste)
+              <Text style={{ paddingHorizontal: 25, fontSize: 20 }}> value={placarT1}
+                {placarT1}
               </Text>
 
-              <Text style={{ paddingHorizontal: 25, fontSize: 20 }}>
-                (Teste)
+              <Text style={{ paddingHorizontal: 25, fontSize: 20 }}> value={placarT2}
+                {placarT2}
               </Text>
             </View>
 
@@ -75,9 +119,7 @@ export default function App() {
                 Selecione o time do jogador:
               </Text>
               {/* INCOMPLETO, FALTA A PARTE DE SELECIONAR iNFO OU AGRO */}
-              <TouchableOpacity style={styles.button} onPress={onPress}>
-                  <Text>Press Here</Text>
-              </TouchableOpacity>
+             
             </View>
 
             {/* Cadastrar um jogador e sua camiseta */}
@@ -87,13 +129,13 @@ export default function App() {
               </Text>
 
               {/* Input 4 - NOME DO JOGADOR */}
-              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }}
+              <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }} value={nome}
                 placeholder="Nome do jogador"
               />
 
-              {/* Input 5 - NUMERO DA CIMISETA */}
+              {/* Input 5 - NUMERO DA CAMISETA */}
               <View style={{ paddingVertical: 5 }}>
-                <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }}
+                <TextInput style={{ height: 30, width: "100%", borderWidth: 1 }} value={camiseta}
                   placeholder="Número da camiseta do jogador"
                 />
               </View>
@@ -111,6 +153,7 @@ export default function App() {
               <Text style={{ width: "100%", fontSize: 20, paddingTop: 20 }}>
                 Lista de Jogadores:
               </Text>
+
             </View>
 
             <StatusBar style="light" />
